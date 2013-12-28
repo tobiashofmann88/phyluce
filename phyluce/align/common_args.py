271e967dd830ce1d2f946935c081d0927fc70abc
Brant Faircloth <brant@faircloth-lab.org>
2013-12-28 09:19:01 -0800

    PEP8

--- phyluce/align/common_args.py
+++ phyluce/align/common_args.py
@@ -23,97 +23,98 @@ def shared(sp):
         required=True,
         action=FullPaths,
         type=is_file,
-        help="""The file containing FASTA reads associated with targted loci from """ +
-        """get_fastas_from_match_counts.py"""
+        help="The file containing FASTA reads associated with targeted "
+             "loci from get_fastas_from_match_counts.py"
     )
     sp.add_argument(
         "--output",
         required=True,
         action=CreateDir,
-        help="""The directory in which to store the resulting alignments."""
+        help="The directory in which to store the resulting alignments."
     )
     sp.add_argument(
         "--taxa",
         required=True,
         type=int,
-        help="""Number of taxa expected in each alignment."""
+        help="Number of taxa expected in each alignment."
     )
     sp.add_argument(
         "--output-format",
         choices=["fasta", "nexus", "phylip", "clustal", "emboss", "stockholm"],
         default="nexus",
-        help="""The output alignment format.""",
+        help="The output alignment format.",
     )
     sp.add_argument(
         "--verbosity",
         type=str,
         choices=["INFO", "WARN", "CRITICAL"],
         default="INFO",
-        help="""The logging level to use."""
+        help="The logging level to use."
     )
     sp.add_argument(
         "--log-path",
         action=FullPaths,
         type=is_dir,
         default=None,
-        help="""The path to a directory to hold logs."""
+        help="The path to a directory to hold logs."
     )
     sp.add_argument(
         "--incomplete-matrix",
         dest="notstrict",
         action="store_true",
         default=False,
-        help="""Allow alignments that do not contain all --taxa."""
+        help="Allow alignments that do not contain all --taxa."
     )
     sp.add_argument(
         "--no-trim",
         action="store_true",
         default=False,
-        help="""Align, but DO NOT trim alignments."""
+        help="Align, but DO NOT trim alignments."
     )
     sp.add_argument(
         "--window",
         type=int,
         default=20,
-        help="""Sliding window size for trimming."""
+        help="Sliding window size for trimming."
     )
     sp.add_argument(
         "--proportion",
         type=float,
         default=0.65,
-        help="""The proportion of taxa required to have sequence at alignment ends."""
+        help="The proportion of taxa required to have sequence at "
+             "alignment ends."
     )
     sp.add_argument(
         "--threshold",
         type=float,
         default=0.65,
-        help="""The proportion of residues required across the window in """ +
-        """proportion of taxa."""
+        help="The proportion of residues required across the window in "
+             "proportion of taxa."
     )
     sp.add_argument(
         "--max-divergence",
         type=float,
         default=0.20,
-        help="""The max proportion of sequence divergence allowed between any row """ +
-        """of the alignment and the alignment consensus."""
+        help="The max proportion of sequence divergence allowed between any "
+             "row of the alignment and the alignment consensus."
     )
     sp.add_argument(
         "--min-length",
         type=int,
         default=100,
-        help="""The minimum length of alignments to keep."""
+        help="The minimum length of alignments to keep."
     )
     sp.add_argument(
         "--ambiguous",
         action="store_true",
         default=False,
-        help="""Allow reads in alignments containing N-bases."""
+        help="Allow reads in alignments containing N-bases."
     )
     sp.add_argument(
         "--cores",
         type=int,
         default=1,
-        help="""Process alignments in parallel using --cores for alignment. """ +
-        """This is the number of PHYSICAL CPUs."""
+        help="Process alignments in parallel using --cores for alignment. "
+        "This is the number of PHYSICAL CPUs."
     )
     return sp
