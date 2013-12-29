c402bc196f8d7b2a49f0ca5ec93200def60cfafd
Brant Faircloth <brant@faircloth-lab.org>
2013-12-28 17:00:23 -0800

    add props for conda CLI as inspiration for our CLI

--- phyluce/cli/__init__.py
+++ phyluce/cli/__init__.py
@@ -9,6 +9,16 @@ This code is distributed under a 3-clause BSD license. Please see
 LICENSE.txt for more information.
 
 Created on 26 December 2013 16:12 PST (-0800)
+
+NOTE:  The layout of the cli subdirectory of this package draws heavily from
+the "conda" package manager for the anaconda package distribution.  I have
+followed the general code structure used for the CLI of the conda package
+manager because it is excellent, and the structure of the code aligns well
+with a complex CLI interface.
+
+The conda package manger is (c) 2012 Continuum Analytics, Inc.
+http://continuum.io and available under a 3-clause BSD license.
+
 """
 
 
